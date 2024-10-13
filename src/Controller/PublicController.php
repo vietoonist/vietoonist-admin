ffb3b9e84c539b810d0b7c32578a68d5ecb05a6a
Chan Dinh <dinhhoangbaochan@gmail.com>
2024-10-13 17:59:55 +0700

    feature: add sample structure for homepage

--- src/Controller/PublicController.php
+++ src/Controller/PublicController.php
@@ -12,6 +12,50 @@ class PublicController extends AbstractController
   #[Route(path:"/", name:"")]
   public function renderMainPage() : Response
   {
-    dd($this);
+    return $this->render("home.html.twig", [
+      "featured_comics" => array(
+        [
+          "title" => "Superman",
+          "slug" => "",
+          "cover_image_url" => "https://img.com",
+          "description" => "just a simple story",
+        ],
+        [
+          "title" => "Superman 2",
+          "slug" => "",
+          "cover_image_url" => "https://img.com",
+          "description" => "just a simple story",
+        ],
+        [
+          "title" => "Superman 3",
+          "slug" => "",
+          "cover_image_url" => "https://img.com",
+          "description" => "just a simple story",
+        ]
+      ),
+      "featured_stories" => array(
+        [
+          "title" => "Sherlock Holmes",
+          "slug" => "",
+          "cover_image_url" => "https://img.com",
+          "description" => "just a simple story",
+          "content" => "Why do you care?"
+        ],
+        [
+          "title" => "Sherlock Holmes 2",
+          "slug" => "",
+          "cover_image_url" => "https://img.com",
+          "description" => "just a simple story",
+          "content" => "Why do you care?"
+        ],
+        [
+          "title" => "Sherlock Holmes 3",
+          "slug" => "",
+          "cover_image_url" => "https://img.com",
+          "description" => "just a simple story",
+          "content" => "Why do you care?"
+        ],
+      )
+    ]);
   }
 }
